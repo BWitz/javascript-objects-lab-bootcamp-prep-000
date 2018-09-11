3c6fba653017eb756ec29aef9426c5b56e3a2d07
BWitz <bhorowitz2014@gmail.com>
2018-09-11 03:30:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  delete recipes.prop;
+  delete recipes.ingredients;
   return recipes;
 }
