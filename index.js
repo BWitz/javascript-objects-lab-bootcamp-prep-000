f1de209ed5c7568b5995009e2625887fa3ce23f7
BWitz <bhorowitz2014@gmail.com>
2018-09-11 03:42:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  recipes.prop = "1";
-  recipes.prop = "2";
+  recipes.prop = "1",
+  recipes.prop = "2"
   
   return recipes;
 }
